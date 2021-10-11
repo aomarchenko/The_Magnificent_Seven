4bb11c44319a4096f929036aa04bcdf63f901864
Ilja <tadden12@gmail.com>
2021-10-11 17:25:41 +0300

    final corrections

--- src/js/my-library.js
+++
@@ -1,9 +0,0 @@
-const library = document.querySelector('.library-js');
-
-
-
-library.addEventListener("click", onClick);
-
-function onClick(params) {
-    library.classList.add('library_current');
-}
