f006373649417ce04813b8fa6e77498a65aa95cf
Iryna <irasokolova25@ukr.net>
2021-10-11 13:37:05 +0300

    Update home-page.js

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,20 +1,18 @@
 import movieCardTpl from '../templates/movie-card.hbs';
 import dataBaseApi from './searchApi.js';
+import genres from './genres.json';
 const movieList = document.querySelector('.page-film');
 // console.log(movieList);
-
+// console.log(genres);
 const {homePageFetch } = dataBaseApi;
 // console.log(homePageFetch);
-
 
 function createMovieGalleryTpl(){
     const results = homePageFetch();
     results.then(array => {
-        // array.forEach(result =>{
-            // console.log(array);
+        console.log(array);
         movieList.insertAdjacentHTML('beforeend', movieCardTpl(array))
     }) 
-    // })
     return
 }
 createMovieGalleryTpl()
