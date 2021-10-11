1b2cb4845f50d23afa24af0d0037899197780474
Ilja <tadden12@gmail.com>
2021-10-11 15:24:48 +0300

    refs add

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,3 +1,9 @@
-const refs = {
-    library: document.querySelector('.library-js'),
+const library = document.querySelector('.library-js');
+
+
+
+library.addEventListener("click", onClick);
+
+function onClick(params) {
+    library.classList.add('library_current');
 }
