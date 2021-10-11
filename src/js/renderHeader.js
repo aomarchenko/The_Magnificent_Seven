9951efe15a6fe1d78c96224eb5468b5166f0b6c6
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2021-10-11 22:28:20 +0300

    animation,js adptive

--- src/js/renderHeader.js
+++ src/js/renderHeader.js
@@ -7,17 +7,34 @@ const formRef = document.querySelector('.search-input');
 const buttonSearchRef = document.querySelector('.search-button');
 
 
-
 const renderLibary = e => {
     e.preventDefault();
+
     const markUp = libaryHeader();
+
     inputRef.insertAdjacentHTML('beforebegin', markUp);
     headerRef.classList.add('header-library')
     pageHomeBtn.classList.remove('current');
     pageLibaryBtn.classList.add('library_current')
+    pageLibaryBtn.classList.add('animation')
     formRef.classList.add('opacity')
     buttonSearchRef.classList.add('display-none')
     inputRef.classList.add('display-none')
+
+    pageLibaryBtn.removeEventListener('click', renderLibary);
+};
+
+function removeEvent(e) {
+    pageLibaryBtn.removeEventListener('click', renderLibary);
 }
 
-pageLibaryBtn.addEventListener('click', renderLibary)
+
+function renderPageLibary(e){
+    if (headerRef.classList.contains('header-library')) {
+        e.preventDefault();
+        return;
+    }
+    renderLibary(e);
+    removeEvent(e);
+}
+pageLibaryBtn.addEventListener('click', renderPageLibary)
