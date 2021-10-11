4cf2b22914c8caf904319c8f15920bb6651453c4
Iryna <irasokolova25@ukr.net>
2021-10-11 13:37:12 +0300

    Update searchApi.js

--- src/js/searchApi.js
+++ src/js/searchApi.js
@@ -2,7 +2,7 @@ const API_KEY = 'aa19f289e41f4e3ac70c0546f19e5928';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const trendingRequest = 'trending/movie/week';
 const movieRequest = 'search/movie';
-const genreRequest = 'genre/movie/list';
+// const genreRequest = 'genre/movie/list';
 const page = 1;
 
 export default {
@@ -12,8 +12,6 @@ async homePageFetch(){
         // console.log(data);
     let result = await data.results
         // console.log(result);
-    // let img = await result.poster_path
-    // console.log(result.poster_path);   
         return result;
 },
 
@@ -23,21 +21,17 @@ async searchMovieFetch(){
         // console.log(data);
     let result = await data.results
         // console.log(result);
-    
         return result;
 },
 
-async genreFetch(){
-    let response = await fetch(`${BASE_URL}${genreRequest}?api_key=${API_KEY}&language=en-US`)
-    let data = await response.json();
-        console.log(data);
-    let result = await data.genres
-        console.log(result);
-    
-        return result; 
-}
+// async genreFetch(){
+//     let response = await fetch(`${BASE_URL}${genreRequest}?api_key=${API_KEY}&language=en-US`)
+//     let data = await response.json();
+//         console.log(data);
+//     let result = await data.genres
+//         console.log(result);
+//         return result; 
+// }
 
 
 }
-
-// console.log(searchMovieFetch());
