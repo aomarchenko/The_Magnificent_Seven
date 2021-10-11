83e2ed5486b9ac36ae3d0a1306eb0c0d2c05d40c
Iryna <irasokolova25@ukr.net>
2021-10-11 18:21:33 +0300

    Create dataBaseApi.js

---
+++ src/js/dataBaseApi.js
@@ -0,0 +1,37 @@
+const API_KEY = 'aa19f289e41f4e3ac70c0546f19e5928';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const trendingRequest = 'trending/movie/week';
+
+
+export default class ВataBaseApi{
+    constructor(){
+this.page = 1;
+this.request = '';
+    }
+async homePageFetch(){         
+    let response = await fetch(`${BASE_URL}${trendingRequest}?api_key=${API_KEY}`);
+    let data = await response.json();
+        // console.log(data);
+    let result = await data.results
+        // console.log(result);
+        return result;
+}
+
+async searchMovieFetch(){         
+    let response = await fetch(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${this.request}`)
+    let data = await response.json();
+        // console.log(data);
+        //     console.log(result);
+        this.incrementPage()
+        return data;
+}
+incrementPage(){
+    this.page +=1
+    
+}
+ 
+clearPage(){
+    return this.page = 1;
+}
+
+}
