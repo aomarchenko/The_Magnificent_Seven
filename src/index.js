90b231a844c876eb82e18256e91892c462177879
Iryna <irasokolova25@ukr.net>
2021-10-11 11:17:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './sass/main.scss';
 
 import 'animate.css/animate.css';
 
-import './js/creatTheme'
+import './js/creatTheme';
 
+import './js/home-page';
 
+import './js/searchApi';
+
+import './js/movie-search';
