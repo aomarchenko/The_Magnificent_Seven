b102cf826f6fad1d73e8140e0f75bcd24f8e7e80
Iryna <irasokolova25@ukr.net>
2021-10-11 11:17:24 +0300

    Update home-page.js

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,17 +1,17 @@
 import movieCardTpl from '../templates/movie-card.hbs';
-import homePageFetch from './dataBaseApi.js';
-
+import dataBaseApi from './searchApi.js';
 const movieList = document.querySelector('.page-film');
 // console.log(movieList);
 
-
+const {homePageFetch } = dataBaseApi;
+// console.log(homePageFetch);
 
 
 function createMovieGalleryTpl(){
     const results = homePageFetch();
     results.then(array => {
         // array.forEach(result =>{
-            console.log(array);
+            // console.log(array);
         movieList.insertAdjacentHTML('beforeend', movieCardTpl(array))
     }) 
     // })
