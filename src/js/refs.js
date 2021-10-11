f0911ba9c782df25d1667a4a24dfb5abc116bb86
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2021-10-11 20:50:37 +0300

    ok

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+const inputRef = document.querySelector('.search-form');
+const pageHomeBtn = document.querySelector('.js-button-home')
+const pageLibaryBtn = document.querySelector('.js-button-libary')
+const headerRef = document.querySelector('.header');
+const formRef = document.querySelector('.search-input');
+const buttonSearchRef = document.querySelector('.search-button');
