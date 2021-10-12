f3883bd8f7b66ca6e15bae821f70b9473a912eb7
Volodymyr Dvorskyi <feowurd@gmail.com>
2021-10-12 12:55:50 +0300

    Добавляет открытие модалки с нужным фильмом

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -3,27 +3,40 @@ import filmCardTmpl from '../templates/modal-window-tpl.hbs'
 const closeModalBtn = document.querySelector('.modal__button-close');
 const modalWindow = document.querySelector('.backdrop');
 const aboutFilmContainer = document.querySelector('.modal__content')
+const filmsContainer = document.querySelector('.page-film');
 
 
-
+filmsContainer.addEventListener('click', modalOpener)
 
 closeModalBtn.addEventListener('click', closeModalFn);
 
 function closeModalFn  () {
-    modalWindow.classList.add('is-hidden')
+    modalWindow.classList.add('is-hidden');
+    aboutFilmContainer.innerHTML = ""
  }
 
+function modalOpener(e) {
+    e.preventDefault()
+    const id = e.target.dataset.id;
+    console.dir(id);
+    if(e.target.tagName === 'IMG') {
+        modalWindow.classList.remove('is-hidden');
+        fetchFilm(id);
+    }
+    
+}
 
-fetch(`https://api.themoviedb.org/3/movie/11?api_key=aa19f289e41f4e3ac70c0546f19e5928`)
-.then(res => {return res.json()})
-.then(film=> {
-    const markup = filmCardTmpl(film);
-    aboutFilmContainer.innerHTML = markup;
-})
+function fetchFilm(id) {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=aa19f289e41f4e3ac70c0546f19e5928`)
+    .then(res => {return res.json()})
+    .then(film=> {
+        const markup = filmCardTmpl(film);
+        aboutFilmContainer.innerHTML = markup;
+    })
+}
 
-// fetch(`https://api.themoviedb.org/3/configuration?api_key=aa19f289e41f4e3ac70c0546f19e5928`)
-// .then(res => {return res.json()})
-// .then(film=> {
-//     console.log(film);
-// })
-
+// const fetchFilmDetails = async (id) => {
+//     const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=aa19f289e41f4e3ac70c0546f19e5928`);
+//     const film = await res.json;
+//     return film
+// }
