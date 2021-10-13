4a10d5b9d1adc336d78c26f23ef591ce4ca1e8b4
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2021-10-13 12:36:39 +0300

    Render ok

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,3 +1,5 @@
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import movieCardTpl from '../templates/movie-card.hbs';
 import DataBaseApi from './dataBaseApi.js';
 import genres from './genres.json';
@@ -8,12 +10,19 @@ const dataBaseApi = new DataBaseApi();
 // console.log(homePageFetch);
 
 
-function createMovieGalleryTpl(){
+function createMovieGalleryTpl() {
+        NProgress.start();
+    NProgress.configure({ ease: 'ease', speed: 800 });
+    NProgress.configure({ trickleRate: 0.02, trickleSpeed: 500 });
     const results = dataBaseApi.homePageFetch();
     results.then(array => {
         // console.log(array);
         movieList.insertAdjacentHTML('beforeend', movieCardTpl(array))
+         NProgress.done();
     }) 
     return
 }
 createMovieGalleryTpl()
+export {
+    createMovieGalleryTpl,
+}
