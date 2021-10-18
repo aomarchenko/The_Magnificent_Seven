c2d5369cfc9000e9964bfaa54fd1f375b509e601
Ilja <tadden12@gmail.com>
2021-10-18 22:12:25 +0300

    setup fetch by id

--- src/js/render/renderLibrary.js
+++ src/js/render/renderLibrary.js
@@ -1,12 +1,56 @@
-import libraryRender from '../../templates/clear-library.hbs'
+import libraryMistake from '../../templates/clear-library.hbs'
 import {
     galleryTps,
     pageLibaryBtn,
+    watchedBtn,
+    queueBtn,
 } from '../refs/refs'
+
+const watchedId = JSON.parse(localStorage.getItem('watch'));
+// console.log(watchedId);
+
+const queueId = localStorage.getItem('queue');
+
+
+pageLibaryBtn.addEventListener('click', libraryRender);
+
+
+
+
+function fetchFilm(id) {
+  fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=aa19f289e41f4e3ac70c0546f19e5928`)
+    .then(res => {
+      return res.json();
+    });
+}
+
+function clickLibraryBtn() {
+  galleryTps.innerHTML = '';
+}
 
 function refreshPage() {
   document.location.reload();
 }
+
+// function watchedRender(e) {
+//   e.preventDefault(); 
+//   fetchFilm(watchedId);
+// }
+
+function libraryRender(e) {  
+  clickLibraryBtn();
+  if (watchedId) {
+    fetchFilm(watchedId);
+  }
+  else {
+    const clearMarkup = libraryMistake();
+    galleryTps.insertAdjacentHTML('beforeend', clearMarkup);
+    const btnGoHome = document.querySelector('.library-button');
+    btnGoHome.addEventListener('click', refreshPage);
+  }
+}
+
+
 
 // function clickWatched(btnWatchedLib, btnQueueLib) {
 //   btnWatchedLib.addEventListener('click', renderWatched);
@@ -47,21 +91,4 @@ function refreshPage() {
 //   } 
 
 // }
-
-pageLibaryBtn.addEventListener('click', plugLib);
-function clickLibraryBtn() {
-    galleryTps.innerHTML = '';
-}
-
-function plugLib() {
-clickLibraryBtn()
-  const clearMarkup = libraryRender();
-
-  galleryTps.insertAdjacentHTML('beforeend', clearMarkup);
-
-  const btnGoHome = document.querySelector('.library-button');
-  btnGoHome.addEventListener('click', refreshPage);
-
-}
-
 
