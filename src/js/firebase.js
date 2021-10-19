8ae951bcf3f38eb5e62baa7340a5239cdb9cc1a4
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-19 15:40:32 +0300

    almost done

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import notification from './notification';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCoK0KbVR5F9Gbl47XCVO_8gFrP9bTN0K0',
@@ -13,37 +14,55 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
-let email = 'timetravel@ukr.net';
-let password = 'password';
 
-createUserWithEmailAndPassword(auth, email, password)
-  .then(userCredential => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch(error => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-
-signInWithEmailAndPassword(auth, email, password)
-  .then(userCredential => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch(error => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
 const loginBacdrop = document.querySelector('.login__backdrop');
 const body = document.querySelector('body');
 const closeLoginBtn = document.querySelector('.login__modal--btn-close');
 const loginLink = document.querySelector('.js-button-login');
+const loginForm = document.querySelector('.login');
+const loginButton = document.querySelector('.login_modal--btn-login');
+const createAccountButton = document.querySelector('.login_modal--btn-create-acnt');
 
+loginForm.addEventListener('click', onSubmit);
 loginLink.addEventListener('click', openModal);
+
+function onSubmit(e) {
+  e.preventDefault(e);
+  //   console.log(e.target);
+  let email = e.currentTarget.elements.mailfield.value;
+  let password = e.currentTarget.elements.passwordfield.value;
+  if (e.target === createAccountButton) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        createUserSucces();
+        console.log('ok');
+      })
+
+      .catch(error => {
+        notification.loginError();
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  } else if (e.target === loginButton) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+
+        console.log('ok');
+      })
+      .catch(error => {
+        notification.loginError();
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+}
 
 function openModal(event) {
   event.preventDefault();
