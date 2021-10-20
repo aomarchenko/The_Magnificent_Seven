6413a33665513f352fa5d7c2d066fae575f5b285
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-20 13:27:53 +0300

    1

--- src/index.js
+++ src/index.js
@@ -8,23 +8,17 @@ import './js/home-page';
 import './js/dataBaseApi';
 import './js/movie-search';
 
-
 import './js/components/back-to-top';
 
 import './js/team';
 import './js/modal-window';
 import './js/notification';
 
-
-
-
-
-import './js/render/renderLibraryMy'
+import './js/render/renderLibraryMy';
 
 import './js/render/renderHeader';
 import './js/render/renderLibrary';
 
-
 import './js/localStorage';
 import './js/localStoragePage';
-import './js/firebase';
+// import './js/firebase';
